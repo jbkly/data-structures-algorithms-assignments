92a2fc15dd51d99e544ac51f645e9c2327b7da56
Jordan Buckley <jordan@bkly.org>
2014-09-02 19:50:24 -0700

    exercise 1-1

---
+++ ex1-1.js
@@ -0,0 +1,19 @@
+function Grades() {
+  this.content = [];
+}
+
+Grades.prototype.addGrade = function(grade) {
+  this.content.push(grade);
+  console.log('added ' + grade + ' to grades');
+}
+
+Grades.prototype.getAverage = function() {
+  if (this.content.length === 0) return null;
+  var sum = this.content.reduce(function (a, b) {
+    return +a + +b;
+  });
+  var avg = sum / this.content.length;
+  console.log('grade average: ' + avg); 
+}
+
+module.exports = Grades;
